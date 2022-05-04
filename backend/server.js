de75477b07f2980697d1d603e8f89172b71a07b4
unknown <lsr488@gmail.com>
2022-05-04 09:42:18 -0400

    Add userRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
+// INITIALIZE APP
 const app = express();
 
+// ROOT ROUTE
+app.get('/', (req, res) => {
+	res.status(200).send('Hello');
+})
+
+// ROUTES
+app.use('/api/users', require('./routes/userRoutes'));
+
+// SERVER
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
