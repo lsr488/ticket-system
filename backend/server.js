1acef7c423975e7823fd70d9f6be89457b670e9a
unknown <lsr488@gmail.com>
2022-05-05 08:10:27 -0400

    Add JSON encoding and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 8000;
 
 // INITIALIZE APP
 const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
@@ -12,6 +17,9 @@ app.get('/', (req, res) => {
 
 // ROUTES
 app.use('/api/users', require('./routes/userRoutes'));
+
+// MORE MIDDLEWARE
+app.use(errorHandler);
 
 // SERVER
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
