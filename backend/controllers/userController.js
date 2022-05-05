cc9bf3c22d8cd7c2dacda141d3c29206ff09bae7
unknown <lsr488@gmail.com>
2022-05-05 08:11:04 -0400

    Add JSON encoding and error handling

---
+++ backend/controllers/userController.js
@@ -0,0 +1,28 @@
+// @desc		Register a new user
+// @routes	/api/users/
+// @access	Public
+const registerUser = (req, res) => {
+	console.log(req.body);
+
+	const {name, email, password} = req.body;
+
+	// Validation
+	if(!name || ! email || !password) {
+		res.status(400);
+		throw new Error('Please include all fields');
+	}
+
+	res.send('Register Route');
+}
+
+// @desc		Login a user
+// @routes	/api/users/login
+// @access	Public
+const loginUser = (req, res) => {
+	res.send('Login Route');
+}
+
+module.exports = {
+	registerUser,
+	loginUser
+}
