62145b0826dbee7a7908d2d9edbda9c459ba6b37
unknown <lsr488@gmail.com>
2022-05-05 09:00:41 -0400

    Return user data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,7 +73,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @routes	/api/users/me
 // @access	Private
 const getMe = asyncHandler(async (req, res) => {
-	res.send('me');
+	res.status(200).json(req.user);
 })
 
 // Generate web token
