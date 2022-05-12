3969844afe01f379ac27338942d1c8722110e174
unknown <lsr488@gmail.com>
2022-05-12 09:08:31 -0400

    Fixed typo

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getTickets, createTicket} = require('../controllers/ticketConroller')
+const {getTickets, createTicket} = require('../controllers/ticketController')
 
 const {protect} = require('../middleware/authMiddleware')
 
