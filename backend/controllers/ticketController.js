9e3a2b54b5bd3064ae5eb0fe9a7caac91fcae552
unknown <lsr488@gmail.com>
2022-05-12 09:18:26 -0400

    Create a new ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -6,16 +6,46 @@ const Ticket = require('../models/ticketModel')
 // @routes	GET /api/tickets
 // @access	Private
 const getTickets = asyncHandler(async (req, res) => {
+	// get current user using ID in the web token
+	const user = await User.findById(req.user.id)
 
-	res.status(200).json({message: 'getTickets'});
+	// if(!user) {
+	// 	res.status(401)
+	// 	throw new Error('User not found')
+	// }
+
+	const tickets = await Ticket.find({user: req.user.id})
+
+	res.status(200).json(tickets);
 })
 
 // @desc		Create new tickets
 // @routes	POST /api/tickets
 // @access	Private
 const createTicket = asyncHandler(async (req, res) => {
+	const {product, description} = req.body
 
-	res.status(200).json({message: 'createTicket'});
+	if(!product || !description) {
+		res.status(400)
+		throw new Error('Please add a product and description')
+	}
+
+	// get current user using ID in the web token
+	const user = await User.findById(req.user.id)
+
+	// if(!user) {
+	// 	res.status(401)
+	// 	throw new Error('User not found')
+	// }	
+
+	const ticket = await Ticket.create({
+		product,
+		description,
+		user: req.user.id,
+		status: 'new'
+	})
+
+	res.status(201).json(ticket);
 })
 
 module.exports = {
