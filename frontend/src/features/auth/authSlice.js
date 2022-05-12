b794ec5762270bb31595125fdad633e50094b08f
unknown <lsr488@gmail.com>
2022-05-12 07:58:22 -0400

    Initial commit

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,34 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+const initialState = {
+	user: null,
+	isError: false,
+	isSuccess: false,
+	isLoading: false,
+	message: ''
+}
+
+// Register new user
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+	try {
+		return await authService.register(user)
+	} catch(error) {
+		const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+		return thunkAPI.rejectWithValue(message)
+	}
+})
+
+// Login user
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+	console.log(user);
+})
+
+export const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {},
+	extraReducers: (builders) => {}
+})
+
+export default authSlice.reducer
