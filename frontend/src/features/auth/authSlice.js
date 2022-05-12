dabdfa50d5ae6cf7d9020b035b57cbc68c05ab20
unknown <lsr488@gmail.com>
2022-05-12 08:22:31 -0400

    Populate user details from localStorage if exists

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,11 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import authService from './authService'
 
+// get user from locaStorage
+const user = JSON.parse(localStorage.getItem('user'))
+
 const initialState = {
-	user: null,
+	user: user ? user : null,
 	isError: false,
 	isSuccess: false,
 	isLoading: false,
