e2434f722f31289a739732a19f4ed00f3f82eeb9
unknown <lsr488@gmail.com>
2022-05-12 08:43:07 -0400

    Add logout and login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -25,7 +25,18 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
 
 // Login user
 export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
-	console.log(user);
+	try {
+		return await authService.login(user)
+	} catch(error) {
+		const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+		return thunkAPI.rejectWithValue(message)
+	}
+})
+
+// Logout current user
+export const logout = createAsyncThunk('auth/logout', async () => {
+	await authService.logout()
 })
 
 export const authSlice = createSlice({
@@ -53,6 +64,9 @@ export const authSlice = createSlice({
 				state.isLoading = false
 				state.isError = true
 				state.message = action.payload
+				state.user = null
+			})
+			.addCase(logout.fulfilled, (state) => {
 				state.user = null
 			})
 	}
