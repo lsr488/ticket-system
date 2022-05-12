898146ceff67c9537fff610c653f35dc434a7a0a
unknown <lsr488@gmail.com>
2022-05-12 08:16:20 -0400

    Added reducers

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
 
 const initialState = {
 	user: null,
@@ -27,8 +28,32 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
 export const authSlice = createSlice({
 	name: 'auth',
 	initialState,
-	reducers: {},
-	extraReducers: (builders) => {}
+	reducers: {
+		reset: (state) => {
+			state.isLoading = false
+			state.isError = false
+			state.isSuccess = false
+			state.message = ''
+		}
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(register.pending, (state) => {
+				state.isLoading = true
+			})
+			.addCase(register.fulfilled, (state, action) => {
+				state.isLoading = false
+				state.isSuccess = true
+				state.user = action.payload
+			})
+			.addCase(register.rejected, (state, action) => {
+				state.isLoading = false
+				state.isError = true
+				state.message = action.payload
+				state.user = null
+			})
+	}
 })
 
+export const {reset} = authSlice.actions
 export default authSlice.reducer
