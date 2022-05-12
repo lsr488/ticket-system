a0b04abea98c7ee8b04d8da0af032010c9fcaa06
unknown <lsr488@gmail.com>
2022-05-12 09:08:16 -0400

    Add missing comma

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -18,7 +18,7 @@ const ticketSchema = mongoose.Schema({
 	status: {
 		type: String,
 		required: true,
-		enum: ['new', 'open', 'closed']
+		enum: ['new', 'open', 'closed'],
 		default: 'new'
 	}
 }, 
