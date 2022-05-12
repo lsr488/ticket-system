d64913474fb9365dbd064fd1a84aaae6f8c75333
unknown <lsr488@gmail.com>
2022-05-12 09:03:30 -0400

    Bring in ticketController

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const {getTickets, createTicket} = require('../controllers/ticketConroller')
 
 const {protect} = require('../middleware/authMiddleware')
 
