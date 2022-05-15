7733f9c0ff9462180085bed31b71f96b2e0111f7
unknown <lsr488@gmail.com>
2022-05-15 11:40:16 -0400

    Add delete and update ticket routes

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -75,8 +75,69 @@ const createTicket = asyncHandler(async (req, res) => {
 	res.status(201).json(ticket);
 })
 
+// @desc		Delete ticket
+// @routes	DELETE /api/tickets/:id
+// @access	Private
+const deleteTicket = asyncHandler(async (req, res) => {
+	// get current user using ID in the web token
+	const user = await User.findById(req.user.id)
+
+	if(!user) {
+		res.status(401)
+		throw new Error('User not found')
+	}
+
+	const ticket = await Ticket.findById(req.params.id)
+
+	if(!ticket) {
+		res.status(404)
+		throw new Error('Ticket not found')
+	}
+
+	if(ticket.user.toString() !== req.user.id) {
+		res.status(401)
+		throw new Error('Not Authorized')
+	}
+
+	await Ticket.remove()
+
+	res.status(200).json({success: true});
+})
+
+// @desc		Update ticket
+// @routes	PUT /api/tickets/:id
+// @access	Private
+const updateTicket = asyncHandler(async (req, res) => {
+	// get current user using ID in the web token
+	const user = await User.findById(req.user.id)
+
+	if(!user) {
+		res.status(401)
+		throw new Error('User not found')
+	}
+
+	const ticket = await Ticket.findById(req.params.id)
+
+	if(!ticket) {
+		res.status(404)
+		throw new Error('Ticket not found')
+	}
+
+	if(ticket.user.toString() !== req.user.id) {
+		res.status(401)
+		throw new Error('Not Authorized')
+	}
+
+	// { new: true } means the ticket will be created if it doesn't already exist
+	const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+	res.status(200).json(updatedTicket);
+})
+
 module.exports = {
 	getTickets,
 	getTicket,
-	createTicket
+	createTicket,
+	deleteTicket,
+	updateTicket
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getTickets, getTicket, createTicket} = require('../controllers/ticketController')
+const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 
 const {protect} = require('../middleware/authMiddleware')
 
@@ -10,5 +10,7 @@ router.route('/')
 
 router.route('/:id')
 	.get(protect, getTicket)
+	.delete(protect, deleteTicket)
+	.put(protect, updateTicket)
 
 module.exports = router
