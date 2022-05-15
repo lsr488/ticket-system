08bbb309d32c4287cbbb481ea1a9b90e9f66214d
unknown <lsr488@gmail.com>
2022-05-15 12:23:07 -0400

    Create new ticket slice

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import ticketService from './ticketService'
 
 const initialState = {
@@ -9,6 +9,23 @@ const initialState = {
 	isLoading: false,
 	message: ''
 }
+
+// Create new ticket
+export const createTicket = createAsyncThunk('auth/create', async (ticketData, thunkAPI) => {
+	try {
+		const token = thunkAPI.getState().auth.user.token
+		return await ticketService.createTicket(ticketData, token)
+	} catch(error) {
+		const message = 
+			(error.response && 
+				error.response.data && 
+				error.response.data.message) || 
+			error.message || 
+			error.toString()
+
+		return thunkAPI.rejectWithValue(message)
+	}
+})
 
 export const ticketSlice = createSlice({
 	name: 'ticket',
