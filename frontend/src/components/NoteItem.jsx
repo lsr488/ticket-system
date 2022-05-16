c7ae295e398e2f9e719ec03c6cd254095b0be6e3
unknown <lsr488@gmail.com>
2022-05-15 21:05:40 -0400

    Fixed style

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -4,7 +4,7 @@ function NoteItem({note}) {
 	const {user} = useSelector((state) => state.auth)
 	return (
 		<div className="note" style={{
-			backgroundColor: note.isStaff ? 'rbga(0,0,0,0.7)' : '#fff',
+			backgroundColor: note.isStaff ? 'rgba(0,0,0,0.7)' : '#fff',
 			color: note.isStaff ? '#fff' : '#000'
 		}}>
 			<h4>Note from {note.isStaff ? <span>Staff</span> : <span>{user.name}</span>}</h4>
